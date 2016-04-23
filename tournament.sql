a75a9b7d6c34deb35d2aad971d5b9c5e6bcff2d1
Carolyn Gibney <cemgibney@users.noreply.github.com>
2016-04-23 12:06:50 -0500

    Add views for even, odd rows and pairs

--- tournament.sql
+++ tournament.sql
@@ -12,15 +12,15 @@ CREATE DATABASE tournament;
 
 CREATE TABLE players 
 	(
-		player_name text, 
-		player_id serial primary key
+		player_name TEXT, 
+		player_id serial PRIMARY KEY
 	);
 
 CREATE TABLE matches 
 	(
-		player_id integer primary key references players, 
-		winner integer, 
-		match_id integer
+		player_id INTEGER PRIMARY KEY REFERENCES players, 
+		winner INTEGER, 
+		match_id INTEGER
 	);
 
 CREATE VIEW standings 
@@ -30,10 +30,10 @@ AS
 			CASE 
 				WHEN SUM(matches.winner) IS NULL THEN 0 
 				ELSE SUM(matches.winner) 
-				END					  AS wins, 
-				Count(matches.winner) AS matches 
+				END			AS wins, 
+				Count(matches.winner) 	AS matches 
 	FROM 	players, 
-			matches 
+		matches 
 	WHERE 	players.player_id = matches.player_id 
 	GROUP BY players.player_id 
 	ORDER BY wins desc;
@@ -45,3 +45,43 @@ AS
 			ELSE Max(match_id) + 1 
 		END AS match_id 
 	FROM matches;
+
+CREATE VIEW s1
+AS 
+	SELECT 	Row_number()
+			OVER(
+				ORDER BY wins DESC) AS 	position, 
+		*
+	FROM standings;
+
+CREATE VIEW even
+AS
+	(SELECT Row_number()
+			OVER(
+				ORDER BY s1.wins DESC) AS i,
+			s1.player_id,
+			s1.player_name,
+			s1.wins
+	FROM s1
+	WHERE Mod(position, 2) = 0);
+
+CREATE VIEW odd
+AS 
+	(SELECT Row_number()
+			OVER(
+				ORDER BY s1.wins DESC) AS i,
+			s1.player_id,
+			s1.player_name,
+			s1.wins
+	FROM s1
+	WHERE Mod(position, 2) != 0);
+
+CREATE VIEW pairs
+AS 
+	(SELECT	odd.player_id		AS player_1_id, 
+		odd.player_name		AS player_1_name, 
+		even.player_id 		AS player_2_id, 
+		even.player_name 	AS player_2_name
+	FROM	odd,
+		even
+	WHERE 	odd.i = even.i);
